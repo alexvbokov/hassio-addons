b8cb6a24884270f7565de2feb08d6e1cc098cd16
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2022-03-15 17:02:32 +0300

    Add files via upload

---
+++ cloudio/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/with-contenv bashio
+
+
+# Generate key
+KEY_PATH=/data/ssh_keys
+if [ ! -d "$KEY_PATH" ]; then
+    echo "[INFO] Setup private key"
+    mkdir -p "$KEY_PATH"
+
+	ssh-keygen -t ed25519 -N "" -f "${KEY_PATH}/autossh_ed25519"
+else
+    echo "[INFO] Restore private_keys"
+fi
+
+echo "[INFO] public key is:"
+cat "${KEY_PATH}/autossh_ed25519.pub"
+
+echo "[INFO] config is:"
+cat /data/options.json 
+
+
+client_id=$(cat /data/options.json | jq -r ".client_id")
+hassio_ip=$(cat /data/options.json | jq -r ".hassio_ip")
+cloud_hostname='cloud.bokov.msk.ru'
+cloud_username='debian'
+cloud_ssh_port=22
+control_port=$((client_id))
+monitor_port=$((control_port+1))
+
+
+# export AUTOSSH_DEBUG=1
+
+echo "[INFO] testing cloud ssh connection"
+ssh -o StrictHostKeyChecking=no -p $cloud_ssh_port $cloud_hostname 2>/dev/null || true
+
+command_args="-M ${monitor_port} -R 0.0.0.0:${control_port}:${hassio_ip}:8123 -N -q -o ServerAliveInterval=25 -o ServerAliveCountMax=3 ${cloud_username}@${cloud_hostname} -p ${cloud_ssh_port} -i ${KEY_PATH}/autossh_ed25519"
+echo "[INFO] command args: ${command_args}"
+/usr/bin/autossh ${command_args}
+
+
+
