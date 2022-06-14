d6871abfd279ae5f75053fa52f7f350246494c75
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2022-06-14 23:35:10 +0300

    !

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 echo "== $(printenv SUPERVISOR_TOKEN) =="
-wget -q -O- --header="Authorization: Bearer $(printenv SUPERVISOR_TOKEN)" --header="Content-Type: application/json" http://supervisor/core/api/states/zone.Home
+wget -q -O- --header="Authorization: Bearer $(printenv SUPERVISOR_TOKEN)" --header="Content-Type: application/json" http://supervisor/core/api/states/sensor.processor_temperature
 
 hassio_ip=$(ha network info | grep "192.168" | grep "/" | awk '{print $2}' | cut -d/ -f1)
 echo "hassio ip is $hassio_ip"
