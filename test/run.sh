90dc32865bc315fd9f0c0697a6f3a4c8485fd719
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2022-06-14 22:43:35 +0300

    !

--- test/run.sh
+++ test/run.sh
@@ -2,5 +2,6 @@
 
 echo "== $(printenv SUPERVISOR_TOKEN) =="
 curl -X GET -H "Authorization: Bearer $(printenv SUPERVISOR_TOKEN)" -H "Content-Type: application/json" http://supervisor/network/config
+ls -la
 # python3 /test.py
 
