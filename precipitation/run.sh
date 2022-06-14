c3072899bb0ae2aea0a9fd1464df2ec02e0309f8
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2022-06-14 19:39:37 +0300

    1.34

--- precipitation/run.sh
+++ precipitation/run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
 #cat /data/options.json
+echo "/data folder contents"
 ls -l /data
+
+echo "./ folder contents"
+ls -l 
+
 echo "now running..."
 while ((1)); do 
 	python3 /precipitation.py && break
