6bb94fb802647d6cd96c45c4e3e2a56b28051acb
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2022-06-16 15:51:33 +0300

    !

--- cloudio/run.sh
+++ cloudio/run.sh
@@ -24,7 +24,7 @@ router_webui=$(jq -r ".router_webui" /data/options.json)
 curl -s -X GET -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/network/info > /data/networkinfo
 cat /data/networkinfo
 hassio_ip=$( jq -r ".data.interfaces[] | .ipv4.address[]" /data/networkinfo | awk -F/ '{print $1}' )
-router_ip=$( jq -r ".data.interfaces[] | .ipv4.gateway[]" /data/networkinfo )
+#router_ip=$( jq -r ".data.interfaces[] | .ipv4.gateway[]" /data/networkinfo )
 
 
 cloud_hostname='cloud.uzvhost.ru'
