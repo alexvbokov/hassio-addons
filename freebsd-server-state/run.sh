037b8eaeca9c6ef917aae8bfccba61172d67ab92
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-17 00:21:33 +0300

    0.24

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
 
+jq -r '.["seervers"]' < /data/options.json > /data/options-unquoted.json
 printf "[INFO] /data/options.json is:"
-cat /data/options.json 
+cat /data/options-unquoted.json 
 
 # server_ip=$(jq -r ".server" /data/options.json)
 # ssh_login=$(jq -r ".login" /data/options.json)
 # ssh_pass=$(jq -r ".password" /data/options.json)
 
 
-server_count=$(jq length /data/options.json)
+server_count=$(jq length /data/options-unquoted.json)
 
 while true
 do
@@ -17,20 +18,20 @@ do
 	for (( s=0; s<${server_count}; s++ ))
 	do 
 		
-		server_ip=$(jq "keys[${s}]" /data/options.json)
+		server_ip=$(jq "keys[${s}]" /data/options-unquoted.json)
 		printf "[INFO] server_ip: ${server_ip}\n"
 
-		ssh_login=$(jq -r ".[${server_ip}][\"login\"]" /data/options.json)
-		ssh_pass=$(jq -r ".[${server_ip}][\"password\"]" /data/options.json)
+		ssh_login=$(jq -r ".[${server_ip}][\"login\"]" /data/options-unquoted.json)
+		ssh_pass=$(jq -r ".[${server_ip}][\"password\"]" /data/options-unquoted.json)
 		printf "[INFO] login: ${ssh_login} password: ${ssh_pass} \n"
 
-		sensors=$(jq -r ".[${server_ip}][\"sensors\"] | length" /data/options.json)
+		sensors=$(jq -r ".[${server_ip}][\"sensors\"] | length" /data/options-unquoted.json)
 
 		for (( n=0; n<${sensors}; n++ ))
 		do 
 
-			sensor=$(jq -r ".[${server_ip}][\"sensors\"] | keys[${n}]" /data/options.json)
-			command=$(jq -r ".[${server_ip}][\"sensors\"][${sensor}]" /data/options.json)
+			sensor=$(jq -r ".[${server_ip}][\"sensors\"] | keys[${n}]" /data/options-unquoted.json)
+			command=$(jq -r ".[${server_ip}][\"sensors\"][${sensor}]" /data/options-unquoted.json)
 			printf "[INFO] 	sensor: ${sensor} -> command: ${command} \n"
 
 			value=$(${command})
