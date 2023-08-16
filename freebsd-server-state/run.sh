0b3dc4d4a1ebc69ef1581e2432acabe084a91722
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-16 20:51:13 +0300

    0.15

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -8,27 +8,30 @@ server_ip=$(jq -r ".server" /data/options.json)
 ssh_login=$(jq -r ".login" /data/options.json)
 ssh_pass=$(jq -r ".password" /data/options.json)
 
-command1=$(jq -r ".command1" /data/options.json)
-name1=$(jq -r ".name1" /data/options.json)
 
 while true
 do
 
-	echo "loop start..."
+	for n in 1 2 3 4 5
+	do 
 
-	if [ ! -z "$name1" ]; then
-		command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${command1}"
-		echo "[INFO] command: ${command}"
-		value=$(${command})
-		echo "[INFO] value: ${value}"
-		json_data="{\"state\": \"${value}\" }"
-		echo "[INFO] json_data: '${json_data}'"
-		url="http://supervisor/core/api/states/sensor.${name1}"
-		echo "[INFO] url: ${url}"
-		curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
-	fi
-	
-	# http://supervisor/core/api/states/"+entity_id, headers={ "Authorization": "Bearer "+supervisor_token, "content-type": "application/json" }, data=json.dumps({ "state": value, "attributes": {"friendly_name": friendly_name, "unit_of_measurement": unit, "icon": icon } 
+		remote_command=$(jq -r ".command${n}" /data/options.json)
+		sensor_name=$(jq -r ".name${n}" /data/options.json)
+		echo "[INFO] ${n} sensor_name: ${sensor_name} remote_command: ${remote_command}"
+
+		if [ ! -z "$remote_command" ]; then
+			command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${remote_command}"
+			echo "[INFO] command: ${command}"
+			value=$(${command})
+			echo "[INFO] value: ${value}"
+			json_data="{\"state\": \"${value}\" }"
+			echo "[INFO] json_data: '${json_data}'"
+			url="http://supervisor/core/api/states/sensor.${sensor_name}"
+			echo "[INFO] url: ${url}"
+			curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
+		fi
+
+	done
 
 	echo "repeating in 60 sec..."
 	sleep 60
