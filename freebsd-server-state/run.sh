5b65937dc78d87d31c9ab67298da79c8233adb3d
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-16 22:25:03 +0300

    0.19

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -17,23 +17,23 @@ do
 
 		remote_command=$(jq -r ".command${n}" /data/options.json)
 		sensor_name=$(jq -r ".name${n}" /data/options.json)
-		printf "\n[INFO] [=${n}=] sensor_name: ${sensor_name} remote_command: ${remote_command}"
+		printf "\n[INFO] [=${n}=] sensor_name: ${sensor_name} remote_command: ${remote_command}\n"
 
 		if [ ! -z "$remote_command" ]; then
 			command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${remote_command}"
-			printf "[INFO] command: ${command}"
+			printf "[INFO] command: ${command}\n"
 			value=$(${command})
-			printf "[INFO] value: ${value}"
+			printf "[INFO] value: ${value}\n"
 			json_data="{\"state\": \"${value}\" }"
-			printf "[INFO] json_data: '${json_data}'"
+			printf "[INFO] json_data: '${json_data}'\n"
 			url="http://supervisor/core/api/states/sensor.${sensor_name}"
-			printf "[INFO] url: ${url}"
+			printf "[INFO] url: ${url}\n"
 			curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
 		fi
 
 	done
 
-	printf "repeating in 60 sec..."
+	printf "repeating in 60 sec...\n"
 	sleep 60
 	
 done
