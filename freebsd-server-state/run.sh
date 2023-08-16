be4c55df52a9b9ee4ba3e526ec68bc48935c475a
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-16 21:36:09 +0300

    0.18

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 
-echo "[INFO] json config is:"
+printf "[INFO] json config is:"
 cat /data/options.json 
 
 server_ip=$(jq -r ".server" /data/options.json)
@@ -17,23 +17,23 @@ do
 
 		remote_command=$(jq -r ".command${n}" /data/options.json)
 		sensor_name=$(jq -r ".name${n}" /data/options.json)
-		echo "\n[INFO] [=${n}=] sensor_name: ${sensor_name} remote_command: ${remote_command}"
+		printf "\n[INFO] [=${n}=] sensor_name: ${sensor_name} remote_command: ${remote_command}"
 
 		if [ ! -z "$remote_command" ]; then
 			command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${remote_command}"
-			echo "[INFO] command: ${command}"
+			printf "[INFO] command: ${command}"
 			value=$(${command})
-			echo "[INFO] value: ${value}"
+			printf "[INFO] value: ${value}"
 			json_data="{\"state\": \"${value}\" }"
-			echo "[INFO] json_data: '${json_data}'"
+			printf "[INFO] json_data: '${json_data}'"
 			url="http://supervisor/core/api/states/sensor.${sensor_name}"
-			echo "[INFO] url: ${url}"
+			printf "[INFO] url: ${url}"
 			curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
 		fi
 
 	done
 
-	echo "repeating in 60 sec..."
+	printf "repeating in 60 sec..."
 	sleep 60
 	
 done
