30fcfa4f0ebbefaa975f77ac5e9072be08754400
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-16 20:21:29 +0300

    0.13

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -25,9 +25,7 @@ do
 		echo "[INFO] json_data: ${json_data}"
 		url="http://supervisor/core/api/states/sensor.${name1}"
 		echo "[INFO] url: ${url}"
-		curl_args="-s -X POST -d '${json_data}' -H \"Authorization: Bearer ${SUPERVISOR_TOKEN}\" -H \"Content-Type: application/json\" ${url}"
-		echo "[INFO] curl_args: ${curl_args}"
-		curl ${curl_args}
+		curl -s -X POST -d '${json_data}' -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
 	fi
 	
 	# http://supervisor/core/api/states/"+entity_id, headers={ "Authorization": "Bearer "+supervisor_token, "content-type": "application/json" }, data=json.dumps({ "state": value, "attributes": {"friendly_name": friendly_name, "unit_of_measurement": unit, "icon": icon } 
