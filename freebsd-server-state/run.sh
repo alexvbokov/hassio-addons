b385bab95c43e946af86cc371867fdf0880bddb0
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-16 18:45:57 +0300

    0.06

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -21,7 +21,14 @@ do
 		echo "[INFO] command: ${command}"
 		value=$(${command})
 		echo "[INFO] value: ${value}"
+		json_data="{'state': '${value}' }"
+		echo "[INFO] json_data: ${json_data}"
+		url="http://supervisor/core/api/states/${name1}"
+		echo "[INFO] url: ${url}"
+		curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
 	fi
+	
+	# http://supervisor/core/api/states/"+entity_id, headers={ "Authorization": "Bearer "+supervisor_token, "content-type": "application/json" }, data=json.dumps({ "state": value, "attributes": {"friendly_name": friendly_name, "unit_of_measurement": unit, "icon": icon } 
 
 	echo "repeating in 60 sec..."
 	sleep 60
