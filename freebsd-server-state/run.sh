7e38c6ed11bfdcf94bc72e5de9fa8dd90f3dd45e
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-08-21 17:20:19 +0300

    0.41

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
 
-jq -r '.["servers"]' < /data/options.json > /data/options-unquoted.json
-printf "[INFO] /data/options.json is:\n"
-jq < /data/options-unquoted.json 
-
-# server_ip=$(jq -r ".server" /data/options.json)
-# ssh_login=$(jq -r ".login" /data/options.json)
-# ssh_pass=$(jq -r ".password" /data/options.json)
+options_json="/data/options.json"
+servers_config="/data/options-unquoted.json"
 
 
-server_count=$(jq length /data/options-unquoted.json)
+jq -r '.["servers"]' < "$options_json" > "$servers_config"
+printf "[INFO] servers:\n"
+jq < "$servers_config" 
+
+
+server_count=$(jq length "$servers_config")
+printf "[INFO] $server_count servers in list:\n"
+
 
 while true
 do
@@ -18,21 +20,23 @@ do
 	for (( s=0; s<${server_count}; s++ ))
 	do 
 		
-		server_ip=$(jq -r "keys[${s}]" /data/options-unquoted.json)
+		server_ip=$(jq -r "keys[${s}]" $servers_config)
 		printf "[INFO] server_ip: ${server_ip}\n"
 
-		ssh_login=$(jq -r ".[\"${server_ip}\"][\"login\"]" /data/options-unquoted.json)
-		ssh_pass=$(jq -r ".[\"${server_ip}\"][\"password\"]" /data/options-unquoted.json)
+		ssh_login=$(jq -r ".[\"${server_ip}\"][\"login\"]" $servers_config)
+		ssh_pass=$(jq -r ".[\"${server_ip}\"][\"password\"]" $servers_config)
 		printf "[INFO] login: ${ssh_login} password: ${ssh_pass} \n"
 
-		sensors=$(jq ".[\"${server_ip}\"][\"sensors\"] | length" /data/options-unquoted.json)
+		sensors=$(jq ".[\"${server_ip}\"][\"sensors\"] | length" $servers_config)
 
 		for (( n=0; n<${sensors}; n++ ))
 		do 
 
-			sensor_name=$(jq -r ".[\"${server_ip}\"][\"sensors\"] | keys[${n}]" /data/options-unquoted.json)
+			sensor_name=$(jq -r ".[\"${server_ip}\"][\"sensors\"] | keys[${n}]" $servers_config)
 			printf "[INFO]    sensor: ${sensor_name}\n"
-			remote_command=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"]" /data/options-unquoted.json)
+			remote_command=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"command\"]" $servers_config)
+			unit=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"unit\"]" $servers_config)
+			icon=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"icon\"]" $servers_config)
 			printf "[INFO]    remote_command: ${remote_command} \n"
 
 			command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${remote_command}"
@@ -40,7 +44,7 @@ do
 
 			value=$(${command})
 			printf "[INFO]    value: ${value}\n"
-			json_data="{\"state\": \"${value}\" }"
+			json_data="{\"state\": \"${value}\", \"attributes\": {\"unit_of_measurement\": \"${unit}\", \"icon\": \"$icon\" } }"
 			printf "[INFO]    json_data: '${json_data}'\n"
 			url="http://supervisor/core/api/states/sensor.${sensor_name}"
 			printf "[INFO]    url: ${url}\n"
