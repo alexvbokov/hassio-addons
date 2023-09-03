45fbcb64ced34e4452c7fbc4fbe4b6b264aaa21b
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2023-09-03 18:57:18 +0300

    0.43

--- freebsd-server-state/run.sh
+++ freebsd-server-state/run.sh
@@ -6,12 +6,12 @@ servers_config="/data/options-unquoted.json"
 
 
 jq -r '.["servers"]' < "$options_json" > "$servers_config"
-printf "[INFO] servers:\n"
+printf "[$(date)] servers:\n"
 jq < "$servers_config" 
 
 
 server_count=$(jq length "$servers_config")
-printf "[INFO] $server_count servers in list:\n"
+printf "[$(date)] $server_count servers in list:\n"
 
 
 while true
@@ -20,34 +20,38 @@ do
 	for (( s=0; s<${server_count}; s++ ))
 	do 
 		
+		datetime=$(date)
+		
 		server_ip=$(jq -r "keys[${s}]" $servers_config)
-		printf "[INFO] server_ip: ${server_ip}\n"
+		printf "[$datetime] server_ip: ${server_ip}\n"
 
 		ssh_login=$(jq -r ".[\"${server_ip}\"][\"login\"]" $servers_config)
 		ssh_pass=$(jq -r ".[\"${server_ip}\"][\"password\"]" $servers_config)
-		printf "[INFO] login: ${ssh_login} password: ${ssh_pass} \n"
+		printf "[$datetime] login: ${ssh_login} password: ${ssh_pass} \n"
 
 		sensors=$(jq ".[\"${server_ip}\"][\"sensors\"] | length" $servers_config)
 
 		for (( n=0; n<${sensors}; n++ ))
 		do 
-
+		
+			datetime=$(date)
+	
 			sensor_name=$(jq -r ".[\"${server_ip}\"][\"sensors\"] | keys[${n}]" $servers_config)
-			printf "[INFO]    sensor: ${sensor_name}\n"
+			printf "[$datetime]    sensor: ${sensor_name}\n"
 			remote_command=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"command\"]" $servers_config)
 			unit=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"unit\"]" $servers_config)
 			icon=$(jq -r ".[\"${server_ip}\"][\"sensors\"][\"${sensor_name}\"][\"icon\"]" $servers_config)
-			printf "[INFO]    remote_command: ${remote_command} \n"
+			printf "[$datetime]    remote_command: ${remote_command} \n"
 
 			command="/usr/bin/sshpass -p ${ssh_pass} /usr/bin/ssh -o StrictHostKeyChecking=no ${ssh_login}@${server_ip} ${remote_command}"
-			printf "[INFO]    command: ${command}\n"
+			printf "[$datetime]    command: ${command}\n"
 
 			value=$(${command}) || true
-			printf "[INFO]    value: ${value}\n"
+			printf "[$datetime]    value: ${value}\n"
 			json_data="{\"state\": \"${value}\", \"attributes\": {\"unit_of_measurement\": \"${unit}\", \"icon\": \"$icon\" } }"
-			printf "[INFO]    json_data: '${json_data}'\n"
+			printf "[$datetime]    json_data: '${json_data}'\n"
 			url="http://supervisor/core/api/states/sensor.${sensor_name}"
-			printf "[INFO]    url: ${url}\n"
+			printf "[$datetime]    url: ${url}\n"
 			curl -s -X POST -d "${json_data}" -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" "${url}"
 			
 		done
